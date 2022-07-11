b49761362650ab957ef64fa3ceba091267a7ffd7
Xiaozhe Yao <askxzyao@gmail.com>
2022-07-11 23:33:41 +0200

    restructure

--- run_all.sh
+++
@@ -1,3 +0,0 @@
-python3 shiftql/repl/cli.py --file=shiftql/examples/vldb_6/cosine.shiftql
-python3 shiftql/repl/cli.py --file=shiftql/examples/vldb_6/linear.shiftql
-python3 shiftql/repl/cli.py --file=shiftql/examples/vldb_6/hybrid.shiftql
--- set-env.sh
+++
@@ -1,1 +0,0 @@
-set -o allexport && source .env && source .myenv && source py-dev.env && source worker-nodocker.env
