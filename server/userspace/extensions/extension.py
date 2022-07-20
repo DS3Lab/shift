53a9641799891a7b2d9a8e70d231c5ff8affaa16
Xiaozhe Yao <askxzyao@gmail.com>
2022-07-20 14:23:12 +0200

    sync

--- server/userspace/extensions/extension.py
+++ server/userspace/extensions/extension.py
@@ -1,11 +1,10 @@
 import os
 
-
 class ShiftExtension():
     def __init__(self, name):
         self.name = name
         self.base_data_path = os.path.join("userspace","extensions","src",self.name, "data")
         os.makedirs(self.base_data_path, exist_ok=True)
         
-    def __call__(self, func):
-        pass
+    def __call__(self, models, task):
+        raise NotImplementedError
--- server/userspace/main.py
+++ server/userspace/main.py
@@ -1,6 +1,7 @@
+import os
 from fastapi import FastAPI
 
-ext_app = FastAPI(title="shift_ext", description="Search Engine for Transfer Learning")
+ext_app = FastAPI(title="shift_ext", description="Extensions API for SHiFT")
 
 @ext_app.get("/exts")
 def get_exts():
@@ -9,3 +10,10 @@ def get_exts():
 @ext_app.post("/exts/{ext_name}")
 def request_ext():
     return {"ext_name": "shift_ext"}
+
+if __name__=="__main__":
+    """
+    Here we register all extensions available in the system.
+    """
+    # available_srcs = os.listdir()
+    base_path = os.environ.get("USERSPACE_LOCATION", ".")
